867ba158d133f4b39981e788e1af16e3d4b99187
TomiiB7 <tomilvl3@gmail.com>
2021-04-22 10:12:34 -0300

    Created databse playlist, updated anime commands

--- cog/anime.py
+++ cog/anime.py
@@ -254,6 +254,49 @@ class Anime(commands.Cog):
             )
             await ctx.send(embed=embed)
 
+    @commands.command(name='bye')
+    async def despedir(self, ctx, member: discord.Member = None):
+        guild = ctx.guild
+        choice = random.choice(gif['bye'])
+
+        if member is not None:
+            embed = discord.Embed(
+                color=discord.Color.blue(),
+                timestamp=datetime.datetime.utcnow(),
+            )
+            embed.add_field(
+                name='¡Adiós!',
+                value=f'**{ctx.author.name}** se despide de **{member.name}**',
+                inline=False
+            )
+            embed.set_image(
+                url=choice
+            )
+            embed.set_footer(
+                text=guild,
+                icon_url=guild.icon_url,
+            )
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(
+                color=discord.Color.blue(),
+                timestamp=datetime.datetime.utcnow(),
+                )
+            embed.add_field(
+                name='¡Sayonara!',
+                value=f'**{ctx.author.name}** saluda a todo mundo',
+                inline=False
+            )
+            embed.set_image(
+                url=choice
+            )
+            embed.set_footer(
+                text=guild,
+                icon_url=guild.icon_url,
+            )
+            await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Anime(bot))
