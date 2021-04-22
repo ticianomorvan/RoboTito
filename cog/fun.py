d98d7ae7603e7fc715dbf1688b8bdcadf590f02a
Ticiano <ticiano.morvan@gmail.com>
2021-04-21 21:59:55 -0300

    updated rr command and its database

--- cog/fun.py
+++ cog/fun.py
@@ -5,7 +5,18 @@ import random
 
 import time
 
+import json
+
 import datetime
+
+
+with open('databases/db_phrases.json') as d:
+    rrd = d.read()
+    rrdata = json.loads(rrd)
+    rrdeath = random.choice(rrdata['rr_death'])
+    rrsafe = random.choice(rrdata['rr_safe'])
+    rrbdeath = random.choice(rrdata['rr.bot_death'])
+    rrbsafe = random.choice(rrdata['rr.bot_safe'])
 
 
 class Fun(commands.Cog):
@@ -16,69 +27,70 @@ class Fun(commands.Cog):
     @commands.command(name='ruletarusa', aliases=['rr'])
     async def ruletarusa(self, ctx, member: discord.Member = None):
         guild = ctx.guild
+        author = ctx.author.name
+        gif = ('https://images.rapgenius.com/'
+               '46bb57d90e7f3692014a769f2c3007cc.380x220x27.gif')
 
         if member is not None:
-            await ctx.send(f'Desafiaste a {member.mention} a jugar a la '
+            await ctx.send(f'Desafiaste a {member.name} a jugar a la '
                            'Ruleta Rusa, ¿aceptará?')
 
-            def check_1(m):
+            def msg_member(m):
                 return m.author == member
 
-            def check_2(m):
+            def msg_author(m):
                 return m.author == ctx.author
 
             confirm = await self.bot.wait_for(
-                'message', check=check_1, timeout=60.0
+                'message', check=msg_member, timeout=60.0
             )
             if confirm.content == 'si':
                 await ctx.send('¡Duelo iniciado!, '
-                               f'{ctx.author.name} tu comienzas.')
-                s1 = await self.bot.wait_for(
-                    'message', check=check_2, timeout=60.0
+                               f'{author} tu comienzas.')
+                s = await self.bot.wait_for(
+                    'message', check=msg_author, timeout=60.0
                 )
-                if s1.content == 'probar':
+                if s.content == 'probar':
                     death = random.randint(0, 3)
                     if death <= 2:
-                        await ctx.send('Te salvaste, le toca a '
-                                       f'{member.mention}')
-                        s2 = await self.bot.wait_for(
-                            'message', check=check_1, timeout=60.0
+                        await ctx.send(str(rrsafe))
+                        time.sleep(1)
+                        await ctx.send(f'Tu sigues, {member.name}')
+                        s = await self.bot.wait_for(
+                            'message', check=msg_member, timeout=60.0
                         )
-                        if s2.content == 'probar':
-                            death_2 = random.randint(0, 2)
-                            if death_2 <= 1:
-                                await ctx.send(
-                                    f'Por poco, {member.mention}. Te toca,'
-                                    f' {ctx.author.mention}'
+                        if s.content == 'probar':
+                            death = random.randint(0, 2)
+                            if death <= 1:
+                                await ctx.send(str(rrsafe))
+                                time.sleep(1)
+                                await ctx.send(f'Tu sigues, {author}')
+                                s = await self.bot.wait_for(
+                                    'message', check=msg_author, timeout=60.0
                                 )
-                                s3 = await self.bot.wait_for(
-                                    'message', check=check_2, timeout=60.0
-                                )
-                                if s3.content == 'probar':
-                                    death_3 = random.randint(0, 1)
-                                    if death_3 == 0:
-                                        await ctx.send(
-                                            'La gracia divina te salva,'
-                                            f' turno de {member.mention}'
-                                        )
-                                        s4 = await self.bot.wait_for(
-                                            'message', check=check_1,
+                                if s.content == 'probar':
+                                    death = random.randint(0, 1)
+                                    if death == 0:
+                                        await ctx.send(str(rrsafe))
+                                        time.sleep(1)
+                                        await ctx.send('Te toca, '
+                                                       f'{member.name}')
+                                        s = await self.bot.wait_for(
+                                            'message', check=msg_member,
                                             timeout=60.0
                                         )
-                                        if s4.content == 'probar':
+                                        if s.content == 'probar':
                                             embed = discord.Embed(
                                                 color=discord.Color.blue(),
                                                 timestamp=datetime.datetime.
                                                 utcnow(),
                                             )
                                             embed.add_field(
-                                                name='MORISTE',
-                                                value=f'{member.mention}'
+                                                name=f'Moriste, {member.name}',
+                                                value=str(rrdeath)
                                             )
                                             embed.set_image(
-                                                url='https://images.rapgenius.'
-                                                'com/46bb57d90e7f3692014a769f2'
-                                                'c3007cc.380x220x27.gif'
+                                                url=gif
                                             )
                                             embed.set_footer(
                                                 text=guild,
@@ -93,13 +105,11 @@ class Fun(commands.Cog):
                                             utcnow(),
                                         )
                                         embed.add_field(
-                                            name='MORISTE',
-                                            value=f'{ctx.author.mention}'
+                                            name=f'Moriste, {author}',
+                                            value=str(rrdeath)
                                         )
                                         embed.set_image(
-                                            url='https://images.rapgenius.com/'
-                                                '46bb57d90e7f3692014a769f2c300'
-                                                '7cc.380x220x27.gif'
+                                            url=gif
                                         )
                                         embed.set_footer(
                                             text=guild,
@@ -107,82 +117,155 @@ class Fun(commands.Cog):
                                         )
                                         await ctx.send(embed=embed)
 
+                                else:
+                                    await ctx.send('Utiliza la palabra '
+                                                   'correcta la próxima vez.')
+
                             else:
                                 embed = discord.Embed(
                                     color=discord.Color.blue(),
                                     timestamp=datetime.datetime.utcnow(),
                                 )
                                 embed.add_field(
-                                    name='MORISTE',
-                                    value=f'{member.mention}'
+                                    name=f'Moriste, {member.name}',
+                                    value=str(rrdeath)
                                 )
                                 embed.set_image(
-                                    url='https://images.rapgenius.com/46bb57d'
-                                        '90e7f3692014a769f2c3007cc.380x220x27'
-                                        '.gif'
+                                    url=gif
                                 )
                                 embed.set_footer(
                                     text=guild,
                                     icon_url=guild.icon_url,
                                 )
                                 await ctx.send(embed=embed)
+                        else:
+                            await ctx.send('No, no creo que esa sea '
+                                           'la palabra que buscas.')
                     else:
                         embed = discord.Embed(
                             color=discord.Color.blue(),
                             timestamp=datetime.datetime.utcnow(),
                         )
                         embed.add_field(
-                            name='MORISTE',
-                            value=f'{ctx.author.mention}'
+                            name=f'Moriste, {ctx.author.name}',
+                            value=str(rrdeath)
                         )
                         embed.set_image(
-                            url='https://images.rapgenius.com/46bb57d90e7f369'
-                                '2014a769f2c3007cc.380x220x27.gif'
+                            url=gif
                         )
                         embed.set_footer(
                             text=guild,
                             icon_url=guild.icon_url,
                         )
                         await ctx.send(embed=embed)
+                else:
+                    await ctx.send('Esa no es la palabra clave.')
+            else:
+                await ctx.send('¿No juegas?, que decepción.')
         else:
             await ctx.send('¡Jugaré contigo!, tienes el primer turno.')
 
             def check(m):
                 return m.author == ctx.author
 
-            sh1 = await self.bot.wait_for(
+            s = await self.bot.wait_for(
                 'message', check=check, timeout=60.0
             )
-            if sh1.content == 'probar':
+            if s.content == 'probar':
                 death = random.randint(0, 3)
                 if death <= 2:
-                    await ctx.send('Te salvaste, me toca.')
+                    await ctx.send(str(rrsafe))
+                    time.sleep(0.5)
+                    await ctx.send('Está bien, me toca.')
                     death = random.randint(0, 2)
                     if death <= 1:
-                        time.sleep(0.5)
-                        await ctx.send('No es mi hora aún, tu turno.')
-                        sh1 = await self.bot.wait_for(
+                        time.sleep(1)
+                        await ctx.send(str(rrbsafe))
+                        s = await self.bot.wait_for(
                             'message', check=check, timeout=60.0
                         )
-                        if sh1.content == 'probar':
+                        if s.content == 'probar':
                             death = random.randint(0, 1)
                             if death == 1:
-                                await ctx.send('Una vez más, tienes suerte')
+                                await ctx.send(str(rrsafe))
+                                time.sleep(1.5)
                                 await ctx.send('Está bien, me toca.')
-                                time.sleep(0.5)
-                                await ctx.send('**MORÍ**, necesito '
-                                               'reparaciones :pensive:')
+                                time.sleep(1.5)
+                                embed = discord.Embed(
+                                    color=discord.Color.blue(),
+                                    timestamp=datetime.datetime.utcnow(),
+                                )
+                                embed.add_field(
+                                    name='Morí.',
+                                    value=str(rrbdeath),
+                                    inline=False,
+                                )
+                                embed.set_image(
+                                    url=gif
+                                )
+                                embed.set_footer(
+                                    text=guild,
+                                    icon_url=guild.icon_url,
+                                )
+                                await ctx.send(embed=embed)
                             else:
-                                await ctx.send('**MORISTE**, '
-                                               '¡los robots ganan!')
+                                time.sleep(1.5)
+                                embed = discord.Embed(
+                                    color=discord.Color.blue(),
+                                    timestamp=datetime.datetime.utcnow(),
+                                )
+                                embed.add_field(
+                                    name=f'Moriste, {author}',
+                                    value=str(rrdeath),
+                                    inline=False,
+                                )
+                                embed.set_image(
+                                    url=gif
+                                )
+                                embed.set_footer(
+                                    text=guild,
+                                    icon_url=guild.icon_url,
+                                )
+                                await ctx.send(embed=embed)
                         else:
                             await ctx.send('Deberías "probar" para jugar.')
                     else:
-                        time.sleep(0.5)
-                        await ctx.send('**MORÍ**, mis circuitos están '
-                                       'muy dañados, alguien que me ayude :(')
+                        time.sleep(1.5)
+                        embed = discord.Embed(
+                            color=discord.Color.blue(),
+                            timestamp=datetime.datetime.utcnow(),
+                        )
+                        embed.add_field(
+                            name='Morí',
+                            value=str(rrbdeath),
+                            inline=False,
+                        )
+                        embed.set_image(
+                            url=gif
+                        )
+                        embed.set_footer(
+                            text=guild,
+                            icon_url=guild.icon_url,
+                        )
+                        await ctx.send(embed=embed)
                 else:
-                    await ctx.send('**MORISTE**, no merecías vivir :D')
+                    embed = discord.Embed(
+                        color=discord.Color.blue(),
+                        timestamp=datetime.datetime.utcnow(),
+                    )
+                    embed.add_field(
+                        name=f'Moriste, {author}',
+                        value=str(rrdeath),
+                        inline=False
+                    )
+                    embed.set_image(
+                        url=gif
+                    )
+                    embed.set_footer(
+                        text=guild,
+                        icon_url=guild.icon_url,
+                    )
+                    await ctx.send(embed=embed)
             else:
                 await ctx.send('Si quieres jugar, escribe "probar".')
 
