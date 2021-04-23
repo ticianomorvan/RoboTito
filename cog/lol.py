274aa997e4ef391da18ae25b8480d1d6eb532217
Ticiano <ticiano.morvan@gmail.com>
2021-04-23 19:20:39 -0300

    unfinished changes on lol.py

--- cog/lol.py
+++ cog/lol.py
@@ -1,13 +1,11 @@
 import discord
 from discord.ext import commands
 
+import datetime
+
 import random
 
 import json
-
-with open('databases/db_champs.json') as f:
-    data = f.read()
-    lol = json.loads(data)
 
 with open('databases/db_leagueoflegends.json') as f2:
     data2 = f2.read()
@@ -19,61 +17,51 @@ class Lol(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='pj')
-    async def elec(self, ctx):
+    @commands.command(name='champpick', aliases=['chpick', 'champick'])
+    async def championpick(self, ctx, *, args):
         guild = ctx.guild
-        choice = random.choice(lol['pj'])
-        embed = discord.Embed(
-                color=discord.Color.blue(),
-        )
-        embed.add_field(
-            name='Tu campeón es...',
-            value=f'**{ctx.author.name}** te tocó {choice}',
-            inline=False
-        )
-        embed.set_footer(
-            text=guild,
-            icon_url=guild.icon_url,
-        )
-        await ctx.send(embed=embed)
+        select = random.randint(1, 155)
+        champ_name = lol_ch['champions'][select]['name']
+        champ_header = lol_ch['champions'][select]['header']
+        champ_square = lol_ch['champions'][select]['icon']
+        champ_ugg = lol_ch['champions'][select]['u.gg']
+        champ_page = lol_ch['champions'][select]['lol']
 
-    @commands.command(name='adc')
-    async def elecc(self, ctx):
-        guild = ctx.guild
-        choice = random.choice(lol['adc'])
-        embed = discord.Embed(
-                color=discord.Color.blue(),
-        )
-        embed.add_field(
-            name='Tu adc es...',
-            value=f'**{ctx.author.name}** te tocó {choice}',
-            inline=False
-        )
-        embed.set_footer(
-            text=guild,
-            icon_url=guild.icon_url,
-        )
-        await ctx.send(embed=embed)
+        if args == 'jg':
+            jungler_id = random.randint(1, 155)
+            for role in lol_ch['champions']:
+                jungler = lol_ch['champions'][jungler_id]['role']
+                if str(jungler) == 'jg':
+                    await ctx.send(lol_ch['champions'][jungler_id]['name'])
+                else:
+                    await ctx.send('no')
 
-    @commands.command(name='pk2')
-    async def pk(self, ctx):
-        guild = ctx.guild
-        champ = random.randint(1, 155)
-        embed = discord.Embed(
-            color=discord.Color.blue()
-        )
-        embed.add_field(
-            name='El campeón que escogí para ti es:',
-            value=lol_ch['champions'][champ]['name']
-        )
-        embed.set_thumbnail(
-            url=lol_ch['champions'][champ]['icon']
-        )
-        embed.set_footer(
-            text=guild,
-            icon_url=guild.icon_url
-        )
-        await ctx.send(embed=embed)
+        elif args == 'random':
+            embed = discord.Embed(
+                title=champ_name,
+                description=champ_header,
+                color=discord.Color.blue(),
+                timestamp=datetime.datetime.utcnow(),
+            )
+            embed.set_thumbnail(
+                url=champ_square
+            )
+            embed.add_field(
+                name='Builds, hechizos y estadísticas:',
+                value=f'[Sitio oficial de u.gg]({champ_ugg})',
+                inline=False
+            )
+            embed.add_field(
+                name='Más información sobre el campeón:',
+                value='[Página oficial de League Of Legends]'
+                      f'({champ_page})',
+                inline=False,
+            )
+            embed.set_footer(
+                text=guild,
+                icon_url=guild.icon_url
+            )
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
