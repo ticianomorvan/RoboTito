1cd8d13fccb4ac72df28d651351a84bed66b192e
Ticiano <ticiano.morvan@gmail.com>
2021-05-22 15:03:47 -0300

    working on translating commands

--- cog/translate.py
+++ cog/translate.py
@@ -11,10 +11,55 @@ import json
 
 # Remaking in progress
 
-with open('databases/db_ttlocales.json', encoding='utf-8') as f:
+with open('databases/iso639-1/db_iso639-1.json', encoding='utf-8') as f:
     data = f.read()
     lang = json.loads(data)
-    locales = lang['languages']
+
+with open('databases/db_ttlocales.json') as f2:
+    data2 = f2.read()
+    locales = json.loads(data2)
+
+
+def language(message):
+    if message in lang['afrikaans']:
+        langcode = 'af'
+        return langcode
+    elif message in lang['arabic']:
+        langcode = 'ar'
+        return langcode
+    elif message in lang['belarusian']:
+        langcode = 'be'
+        return langcode
+    elif message in lang['bulgarian']:
+        langcode = 'bg'
+        return langcode
+    elif message in lang['bosnian']:
+        langcode = 'bs'
+        return langcode
+    elif message in lang['catalan']:
+        langcode = 'ca'
+        return langcode
+    elif message in lang['czech']:
+        langcode = 'cs'
+        return langcode
+    elif message in lang['welsh']:
+        langcode = 'cy'
+        return langcode
+    elif message in lang['danish']:
+        langcode = 'da'
+        return langcode
+    elif message in lang['german']:
+        langcode = 'de'
+        return langcode
+    elif message in lang['greek']:
+        langcode = 'el'
+        return langcode
+    elif message in lang['english']:
+        langcode = 'en'
+        return langcode
+    else:
+        if message in locales['languages']:
+            return message
 
 
 class Translate(commands.Cog,
@@ -28,7 +73,7 @@ class Translate(commands.Cog,
                       aliases=['tr', 'tte'],
                       description='Traduce paso a paso.')
     async def translate_normal(self, ctx, extra=None):
-        file = discord.File('images/Translate.png')
+        file = discord.File('images/Translate.png', filename='image.png')
 
         if extra == 'help':
             embed = discord.Embed(title='Ayuda sobre las traducciones',
@@ -51,60 +96,65 @@ class Translate(commands.Cog,
         else:
             await ctx.send('¿Desde qué idioma quieres traducir?')
 
-            def check(m):
-                return m.content in locales
-
             def author(m):
                 return m.author == ctx.message.author
 
             try:
                 msg1 = await self.bot.wait_for('message',
-                                               check=check,
-                                               timeout=30.0)
+                                               check=author,
+                                               timeout=60.0)
 
             except asyncio.TimeoutError:
                 await ctx.send('Tiempo expirado.')
 
             else:
+                fromlang = language(msg1.content)
                 await ctx.send('¡Bien! ahora, ¿a qué idioma quieres '
                                'traducir?')
 
                 try:
                     msg2 = await self.bot.wait_for('message',
-                                                   check=check,
-                                                   timeout=30.0)
+                                                   check=author,
+                                                   timeout=60.0)
 
                 except asyncio.TimeoutError:
                     await ctx.send('Tiempo expirado.')
 
                 else:
-                    translator = Translator(from_lang=msg1.content,
-                                            to_lang=msg2.content)
+                    tolang = language(msg2.content)
+                    translator = Translator(from_lang=fromlang,
+                                            to_lang=tolang)
 
                     await ctx.send('Ya casi, ¿qué quieres traducir?')
 
                     try:
                         msg3 = await self.bot.wait_for('message',
                                                        check=author,
-                                                       timeout=30.0)
+                                                       timeout=60.0)
 
                     except asyncio.TimeoutError:
                         await ctx.send('Solicitud expirada.')
 
+                    except not msg3.content:
+                        await ctx.send('No escribiste nada.')
+
                     else:
-                        translation = translator.translate(msg3.content)
-                        msg2_cont = msg2.content
-                        msg2_upper = msg2_cont.upper()
+                        translation = translator.translate(
+                            msg3.content
+                        )
+                        tolangUpper = tolang.upper()
 
                         embed = discord.Embed(
                             title='¡Traducciones a la orden!',
                             color=discord.Color.blue(),
                             timestamp=datetime.datetime.utcnow(),
                         )
-                        embed.set_thumbnail(url='attachment://Translate.png')
+                        embed.set_thumbnail(
+                            url='attachment://image.png')
                         embed.add_field(name='Traducción:',
-                                        value=f'{translation} ({msg1.content}'
-                                              f'-> {msg2_upper})',
+                                        value=f'{translation}'
+                                              f' ({fromlang}'
+                                              f' -> {tolangUpper})',
                                         inline=True)
                         embed.set_footer(text=ctx.guild,
                                          icon_url=ctx.guild.icon_url)
