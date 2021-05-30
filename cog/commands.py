f7d443146f7e128a18f40b3c942834b03f4dd681
Ticiano <ticiano.morvan@gmail.com>
2021-05-30 13:26:34 -0300

    unused ping&pong and reworked commands in commands.py

--- cog/commands.py
+++ cog/commands.py
@@ -1,7 +1,9 @@
+# import asyncio
+
 import discord
 from discord.ext import commands
 
-# import time
+from discord.member import Member
 
 import datetime
 
@@ -10,19 +12,48 @@ import random
 import json
 
 
-def get_8ball():
-    with open('databases/db_str.json', encoding='utf8') as f:
-        data = f.read()
-        ball8 = json.loads(data)
-        return random.choice(ball8['ball8'])
+with open('databases/db_str.json', encoding='utf8') as f:
+    data = f.read()
+    database = json.loads(data)
 
 
-def get_penis():
-    with open('databases/db_commands.json', encoding='utf-8') as f:
-        data = f.read()
-        penis = json.loads(data)
-        choice = random.choice(penis['penis_command'])
-        return choice
+def get8Ball():
+    return random.choice(database['ball8'])
+
+
+def getPenis(number: int):
+    if number > 1:
+        return random.choice(database['penis'])
+    elif number == 1:
+        return random.choice(database['penis'])
+    else:
+        pass
+
+
+def getLove(number: int):
+    if number <= 45:
+        return random.choice(database['love_low'])
+    elif number >= 46 and number <= 75:
+        return random.choice(database['love_medium'])
+    else:
+        return random.choice(database['love_high'])
+
+
+def getLoveGif(number: int):
+    with open('databases/db_gifs.json', encoding='utf-8') as f:
+        gifData = f.read()
+        gifDatabase = json.loads(gifData)
+        if number >= 65:
+            gif = random.choice(gifDatabase['love_high'])
+            return gif
+        else:
+            gif = random.choice(gifDatabase['love_low'])
+            return gif
+
+
+color = discord.Color.blue()
+
+time = datetime.datetime.utcnow()
 
 
 class Commands(commands.Cog,
@@ -32,106 +63,118 @@ class Commands(commands.Cog,
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='8ball',
-                      description='¿Qué te responderá la bola ocho?')
+    @commands.command(name='8ball')
     async def eightball(self, ctx, *, args):
-
         if args is not None:
-            embed = discord.Embed(colour=discord.Colour.blue(),
-                                  timestamp=datetime.datetime.utcnow())
-            embed.add_field(name='A tu pregunta de...',
-                            value=f'*"{args}"*',
-                            inline=False)
-            embed.add_field(name='La respuesta es:',
-                            value=get_8ball(),
-                            inline=False)
-            embed.set_footer(text=ctx.guild, icon_url=ctx.guild.icon_url)
+            embed = discord.Embed(
+                color=color,
+                timestamp=time
+            )
+            embed.add_field(
+                name=f'**{get8Ball()}**',
+                value=f'*"{args}"*'
+            )
+            embed.set_footer(
+                text=ctx.author.name,
+                icon_url=ctx.guild.icon_url
+            )
 
             await ctx.send(embed=embed)
 
         else:
             await ctx.send('Deberías preguntar algo.')
 
-    @commands.command(aliases=['probabilidad', 'prob'],
-                      description='Comprueba la probabilidad de '
-                                  'que algo suceda.')
+    @commands.command(aliases=['probabilidad', 'prob'])
     async def probability(self, ctx, *, args):
-
         if args is not None:
             probabilidad = random.randint(0, 100)
-            embed = discord.Embed(colour=discord.Color.blue(),
-                                  timestamp=datetime.datetime.utcnow())
-            embed.add_field(name='La probabilidad de...',
-                            value=f'*"{args}"*',
-                            inline=False)
-            embed.add_field(name='Es de un:',
-                            value=f'**{probabilidad}%**',
-                            inline=False)
-            embed.set_footer(text=ctx.guild, icon_url=ctx.guild.icon_url)
+            embed = discord.Embed(
+                color=color,
+                timestamp=time
+            )
+            embed.add_field(
+                name='La probabilidad de...',
+                value=f'"*{args}*"',
+                inline=False
+            )
+            embed.add_field(
+                name='Es de un...',
+                value=f'**{probabilidad}%**',
+                inline=False
+            )
+            embed.set_footer(
+                text=ctx.author.name,
+                icon_url=ctx.guild.icon_url
+            )
 
             await ctx.send(embed=embed)
 
         else:
-            await ctx.send('Debes introducir una pregunta.')
+            await ctx.send('Deberías preguntar algo.')
 
-    @commands.command(name='penis',
-                      aliases=['pene', 'penisize', 'tula'],
-                      description='Conoce el tamaño de tu miembro.')
-    async def penis(self, ctx, member: discord.Member = None):
-        penis = get_penis()
-        g = ctx.guild
+    @commands.command(name='penis', aliases=['pene', 'penisize', 'tula'])
+    async def penis(self, ctx, member: Member = None):
+        penisSize = random.randint(0, 10000)
 
         if member is not None:
-            if penis == 'images/pikapenis.png':
-                file = discord.File('images/pikapenis.png',
-                                    filename='image.png')
+            embed = discord.Embed(
+                color=color,
+                timestamp=time
+            )
+            embed.add_field(
+                name=f'El miembro reproductor de {member.name} mide:',
+                value=f'**{penisSize}** {getPenis(penisSize)}.',
+                inline=False
+            )
+            embed.set_footer(
+                text=ctx.author.name,
+                icon_url=ctx.guild.icon_url
+            )
 
-                embed = discord.Embed(color=discord.Color.blue(),
-                                      timestamp=datetime.datetime.utcnow())
-                embed.add_field(name='¡VOS NO TENÉS ESO AHÍ, ASQUEROS@!',
-                                value='No tengo registro de una '
-                                      'pija tan grande',
-                                inline=False)
-                embed.set_image(url='attachment://image.png')
-                embed.set_footer(text=g, icon_url=g.icon_url)
-
-                await ctx.send(embed=embed, file=file)
-
-            else:
-                embed = discord.Embed(color=discord.Color.blue(),
-                                      timestamp=datetime.datetime.utcnow())
-                embed.add_field(name=f'Este es el miembro de {member.name}:',
-                                value=penis,
-                                inline=False)
-                embed.set_footer(text=g, icon_url=g.icon_url)
-
-                await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
         else:
-            if penis == 'images/pikapenis.png':
-                file = discord.File('images/pikapenis.png',
-                                    filename='image.png')
+            embed = discord.Embed(
+                color=color,
+                timestamp=time
+            )
+            embed.add_field(
+                name='Tu miembro reproductor mide:',
+                value=f'**{penisSize}** {getPenis(penisSize)}.',
+                inline=False
+            )
+            embed.set_footer(
+                text=ctx.author.name,
+                icon_url=ctx.guild.icon_url
+            )
 
-                embed = discord.Embed(color=discord.Color.blue(),
-                                      timestamp=datetime.datetime.utcnow())
-                embed.add_field(name='¡VOS NO TENÉS ESO AHÍ, ASQUEROS@!',
-                                value='No tengo registro de una '
-                                      'pija tan grande',
-                                inline=False)
-                embed.set_image(url='attachment://image.png')
-                embed.set_footer(text=g, icon_url=g.icon_url)
+            await ctx.send(embed=embed)
 
-                await ctx.send(embed=embed, file=file)
+    @commands.command(aliases=['amor', 'loving'])
+    async def love(self, ctx, member: Member = None):
+        loveProbability = random.randint(0, 100)
 
-            else:
-                embed = discord.Embed(color=discord.Color.blue(),
-                                      timestamp=datetime.datetime.utcnow())
-                embed.add_field(name='Este es tu miembro:',
-                                value=penis,
-                                inline=False)
-                embed.set_footer(text=g, icon_url=g.icon_url)
+        if member is not None:
+            e = discord.Embed(
+                    color=color,
+                    timestamp=time
+            )
+            e.add_field(
+                name=f'El amor entre {member.name} y tu es del...',
+                value=f'**{loveProbability}%**, {getLove(loveProbability)}'
+            )
+            e.set_image(
+                url=getLoveGif(loveProbability)
+            ),
+            e.set_footer(
+                text=ctx.author.name,
+                icon_url=ctx.guild.icon_url
+            )
 
-                await ctx.send(embed=embed)
+            await ctx.send(embed=e)
+
+        else:
+            await ctx.send('Deberías mencionar a alguien.')
 
 
 def setup(bot):
