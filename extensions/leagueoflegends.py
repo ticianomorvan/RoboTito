e007faa72f66e5f9725efe67b630acfa77993b94
Aguss-Coder <minecraft20042016@gmail.com>
2021-11-14 07:37:56 -0300

    temporal changes

--- extensions/leagueoflegends.py
+++ extensions/leagueoflegends.py
@@ -1,8 +1,6 @@
 # Nextcord
-import nextcord
 from nextcord.embeds import Embed
 from nextcord.ext import commands
-from discord import Color
 
 # Libraries
 from random import choice, randint
@@ -16,41 +14,21 @@ with open('databases/db_lolchamps.json', encoding='utf-8') as db:
 
 
 # Functions
-def bcolor(ctx: commands.Context):
-    colors = [Color.from_rgb(255, 94, 43),
-              Color.from_rgb(82, 92, 253),
-              Color.from_rgb(72, 159, 181),
-              Color.from_rgb(255, 134, 0)]
+# def gets_name(name: str):
+#    for champ in champs:
+#        if name == champs[champ]['name']:
+#            return champ
+#        else:
+#            pass
+#
+#
+# def gets_random():
 
-    return choice(colors)
-
-def gets_role(role: str):
-    final_role = str.lower(role)
-    list = []
-    for champ in champs:
-        if champs[champ]['roles'][final_role] is True:
-            list.append(champ)
-        else:
-            pass
-
-    return choice(list)
-
-def gets_name(name: str):
-    for champ in champs:
-        if name == champs[champ]['name']:
-            return champ
-        else:
-            pass
-
-def gets_random():
-    index = len(champs)
-    choice = randint(1, index)
-    return choice
-
-def embeds(champ):
+def embeds(ctx: commands.Context, champ):
     index = str(champ)
+    user = ctx.author
     e = Embed(
-        color=nextcord.Colour
+        color=user.color
     )
     e.set_author(
         name='League of Legends champions',
@@ -75,34 +53,77 @@ def embeds(champ):
 
 # Commands
 class Leagueoflegends(commands.Cog):
-    def __init__(self, bot):
-        self.bot: commands.Bot = bot
+    def __init__(self, champ):
+        self.champ: commands.Bot = champ
 
-    @commands.command(
-        aliases=['chname', 'champion']
-        )
-    async def championname(
-        self, ctx: commands.Context, *, name: str
-        ):
-        """Commands related to lol champs"""
-        user = ctx.author
-        champ = gets_name(name)
-        if champ is not None:
-            embeds(champ)
-            await ctx.send(embed=embeds)
+    def get_name(self):
+        return self.champ['name']
+
+    def gets_role(role: str):
+        final_role = str.lower(role)
+        list = []
+        for champ in champs:
+            if champs[champ]['roles'][final_role] is True:
+                list.append(champ)
         else:
-            e = Embed(
-                color=user.color
-            )
-            e.add_field(
-                name='There was a mistake',
-                value="I couldn't recognize that champion,"
-                      " please try again respecting capital"
-                      " letters and symbols. The exact names"
-                      " i use can be found at https://las.op.gg/"
-                      "champion/statistics"
-            )
-            await ctx.send(embed=e)
+            pass
+        return choice(list)
+
+    def get_random(self, champs):
+        index = len(champs)
+        choice = randint(1, index)
+        champion = champs[choice]
+        return champion
+
+    def get_lolpage(self):
+        return self.champ['lolpage']
+
+    def get_description(self):
+        return self.champ['description']
+    
+    def get_header(self):
+        return self.champ['header']
+
+    def embeds(self, champ, ctx: commands.Context):
+        index = champ()
+        user = ctx.author
+        e = Embed(
+            color=user.color
+        )
+        e.set_author(
+            name='League of Legends champions',
+            url=champs[index]['lolpage'],
+            icon_url='assets/lol_icon.png'
+        )
+        e.add_field(
+            name=f"{self.champ['name']}, {self.champ['header']}",
+            value=self.champ['description']
+        )
+
+#
+#    @commands.command(
+#        aliases=['chname', 'champion']
+#        )
+#    async def championname(self, ctx: commands.Context, *, name: str):
+#        """Commands related to lol champs"""
+#        user = ctx.author
+#        champ = gets_name(name)
+#        if champ is not None:
+#            embeds()
+#            await ctx.send(embed=embeds)
+#        else:
+#            e = Embed(
+#                color=user.color
+#            )
+#            e.add_field(
+#                name='There was a mistake',
+#                value="I couldn't recognize that champion,"
+#                      " please try again respecting capital"
+#                      " letters and symbols. The exact names"
+#                      " i use can be found at https://las.op.gg/"
+#                      "champion/statistics"
+#            )
+#            await ctx.send(embed=e)
 
 
 def setup(bot: commands.Bot):
